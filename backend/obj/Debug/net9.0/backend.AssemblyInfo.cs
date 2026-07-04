1b252ae4fca280a13ddc7441d148e97a19cf869f
rasikaprabath12345 <rasikaprabath8694@gmail.com>
2026-07-04 17:58:45 +0530

    Add frontend and backend as normal projects

--- backend/obj/Debug/net9.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net9.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34d10379c93545ed15bac5789e43decb5b775283")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc40fcac93a0a4762135728585ebb1dedb8ce3eb")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
