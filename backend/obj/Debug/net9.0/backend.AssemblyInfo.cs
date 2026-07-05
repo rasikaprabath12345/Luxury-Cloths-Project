f7d95ac4dd8cc954d3bc39eab9b5159dafb50296
rasikaprabath12345 <rasikaprabath8694@gmail.com>
2026-07-05 11:22:15 +0530

    backup project

--- backend/obj/Debug/net9.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net9.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a26f7c1660b7ad247bb9fc1b0649018966b5da4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b39a93773dedb28264b3a35d1f8d7bdbdb4d3f12")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
