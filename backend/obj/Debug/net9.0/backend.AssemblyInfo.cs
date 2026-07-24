872de0ba7f1b8081579ec68b24dcd99d31ff88c8
rasikaprabath12345 <rasikaprabath8694@gmail.com>
2026-07-24 13:25:22 +0530

    update

--- backend/obj/Debug/net9.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net9.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8bc50629264f72af548ce9a5da814ee2ff25f1a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ed57cb6d2036d3573ebe5f109857bd95564c526")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
